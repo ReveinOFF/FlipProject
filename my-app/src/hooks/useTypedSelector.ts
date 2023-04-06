3bcf427e4231de3da4253732977b35bd266fdd96
RonniePlay <57015640+RonniePlay@users.noreply.github.com>
2023-04-06 16:10:45 +0300

    Delete my-app directory

--- my-app/src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from './../store/reducer';
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector; 
